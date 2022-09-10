dd2e8d84428fc6fb7656e6786017585435f15f26
3akev <3akevdev@gmail.com>
2022-09-11 00:46:03 +0200

    Add test for is_game_finished

--- tests/test_win_check.c
+++ tests/test_win_check.c
@@ -6,18 +6,19 @@
 
 MU_TEST(test_is_game_finished_vertical)
 {
-    GameState gamestates[TEST_IS_GAME_FINISHED_VERTICAL_ARRAY_SIZE] = {{{'x', ' ', ' '},
-                                                                        {'x', ' ', ' '},
-                                                                        {'x', ' ', ' '}},
-                                                                       {{' ', 'o', ' '},
-                                                                        {' ', 'o', ' '},
-                                                                        {' ', 'o', ' '}},
-                                                                       {{' ', ' ', 'o'},
-                                                                        {' ', ' ', 'o'},
-                                                                        {' ', ' ', 'o'}},
-                                                                       {{' ', ' ', 'x'},
-                                                                        {' ', ' ', 'o'},
-                                                                        {' ', ' ', 'o'}}};
+    GameState gamestates[TEST_IS_GAME_FINISHED_VERTICAL_ARRAY_SIZE] = {
+        {{'x', ' ', ' '},
+         {'x', ' ', ' '},
+         {'x', ' ', ' '}},
+        {{' ', 'o', ' '},
+         {' ', 'o', ' '},
+         {' ', 'o', ' '}},
+        {{' ', ' ', 'o'},
+         {' ', ' ', 'o'},
+         {' ', ' ', 'o'}},
+        {{' ', ' ', 'x'},
+         {' ', ' ', 'o'},
+         {' ', ' ', 'o'}}};
     int expected[TEST_IS_GAME_FINISHED_VERTICAL_ARRAY_SIZE] = {1, 2, 2, 0};
 
     int i;
@@ -29,18 +30,19 @@ MU_TEST(test_is_game_finished_vertical)
 
 MU_TEST(test_is_game_finished_horizontal)
 {
-    GameState gamestates[TEST_IS_GAME_FINISHED_HORIZONTAL_ARRAY_SIZE] = {{{'x', 'x', 'x'},
-                                                                          {' ', ' ', ' '},
-                                                                          {' ', ' ', ' '}},
-                                                                         {{' ', ' ', ' '},
-                                                                          {'o', 'o', 'o'},
-                                                                          {' ', 'o', ' '}},
-                                                                         {{'x', 'x', 'o'},
-                                                                          {'x', 'o', 'x'},
-                                                                          {'o', 'o', 'o'}},
-                                                                         {{' ', 'x', ' '},
-                                                                          {'o', 'o', 'x'},
-                                                                          {'x', 'x', 'o'}}};
+    GameState gamestates[TEST_IS_GAME_FINISHED_HORIZONTAL_ARRAY_SIZE] = {
+        {{'x', 'x', 'x'},
+         {' ', ' ', ' '},
+         {' ', ' ', ' '}},
+        {{' ', ' ', ' '},
+         {'o', 'o', 'o'},
+         {' ', 'o', ' '}},
+        {{'x', 'x', 'o'},
+         {'x', 'o', 'x'},
+         {'o', 'o', 'o'}},
+        {{' ', 'x', ' '},
+         {'o', 'o', 'x'},
+         {'x', 'x', 'o'}}};
     int expected[TEST_IS_GAME_FINISHED_HORIZONTAL_ARRAY_SIZE] = {1, 2, 2, 0};
 
     int i;
@@ -52,15 +54,16 @@ MU_TEST(test_is_game_finished_horizontal)
 
 MU_TEST(test_is_game_finished_diagonal)
 {
-    GameState gamestates[TEST_IS_GAME_FINISHED_DIAGONAL_ARRAY_SIZE] = {{{'x', ' ', 'x'},
-                                                                        {' ', 'x', ' '},
-                                                                        {' ', ' ', 'x'}},
-                                                                       {{'o', ' ', ' '},
-                                                                        {' ', 'o', 'o'},
-                                                                        {' ', 'o', 'o'}},
-                                                                       {{' ', 'x', ' '},
-                                                                        {'o', 'o', 'x'},
-                                                                        {'x', 'x', 'o'}}};
+    GameState gamestates[TEST_IS_GAME_FINISHED_DIAGONAL_ARRAY_SIZE] = {
+        {{'x', ' ', 'x'},
+         {' ', 'x', ' '},
+         {' ', ' ', 'x'}},
+        {{'o', ' ', ' '},
+         {' ', 'o', 'o'},
+         {' ', 'o', 'o'}},
+        {{' ', 'x', ' '},
+         {'o', 'o', 'x'},
+         {'x', 'x', 'o'}}};
     int expected[TEST_IS_GAME_FINISHED_DIAGONAL_ARRAY_SIZE] = {1, 2, 0};
 
     int i;
@@ -72,18 +75,47 @@ MU_TEST(test_is_game_finished_diagonal)
 
 MU_TEST(test_is_game_finished_antidiagonal)
 {
-    GameState gamestates[TEST_IS_GAME_FINISHED_ANTIDIAGONAL_ARRAY_SIZE] = {{{'x', ' ', 'x'},
-                                                                            {' ', 'x', ' '},
-                                                                            {'x', ' ', ' '}},
-                                                                           {{'o', ' ', 'o'},
-                                                                            {' ', 'o', 'o'},
-                                                                            {'o', 'o', ' '}},
-                                                                           {{' ', 'x', ' '},
-                                                                            {'o', 'o', 'x'},
-                                                                            {'x', 'x', 'o'}}};
+    GameState gamestates[TEST_IS_GAME_FINISHED_ANTIDIAGONAL_ARRAY_SIZE] = {
+        {{'x', ' ', 'x'},
+         {' ', 'x', ' '},
+         {'x', ' ', ' '}},
+        {{'o', ' ', 'o'},
+         {' ', 'o', 'o'},
+         {'o', 'o', ' '}},
+        {{' ', 'x', ' '},
+         {'o', 'o', 'x'},
+         {'x', 'x', 'o'}}};
     int expected[TEST_IS_GAME_FINISHED_ANTIDIAGONAL_ARRAY_SIZE] = {1, 2, 0};
 
     int i;
     for (i = 0; i < TEST_IS_GAME_FINISHED_ANTIDIAGONAL_ARRAY_SIZE; i++)
         mu_assert(is_game_finished_antidiagonal(gamestates[i]) == expected[i], "Error, incorrect win check! (antidiagonal)");
+}
+
+#define TEST_IS_GAME_FINISHED_ARRAY_SIZE 5
+
+MU_TEST(test_is_game_finished)
+{
+    GameState gamestates[TEST_IS_GAME_FINISHED_ARRAY_SIZE] = {
+        {{'x', ' ', 'x'},
+         {' ', 'x', ' '},
+         {'x', ' ', ' '}},
+        {{'o', ' ', ' '},
+         {' ', 'o', 'o'},
+         {' ', 'o', 'o'}},
+        {{' ', 'x', ' '},
+         {'o', 'o', 'x'},
+         {'x', 'x', 'o'}},
+        {{'x', 'x', 'o'},
+         {'x', 'o', 'x'},
+         {'o', 'o', 'o'}},
+        {{'x', ' ', ' '},
+         {'x', ' ', ' '},
+         {'x', ' ', ' '}},
+    };
+    int expected[TEST_IS_GAME_FINISHED_ARRAY_SIZE] = {1, 2, 0, 2, 1};
+
+    int i;
+    for (i = 0; i < TEST_IS_GAME_FINISHED_ARRAY_SIZE; i++)
+        mu_assert(is_game_finished(gamestates[i]) == expected[i], "Error, incorrect win check!");
 }
